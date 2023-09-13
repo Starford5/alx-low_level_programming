13e741533ddbc2bc11347761941e26250985a453
Starford5 <ikechukwustarford@gmail.com>
2023-09-12 22:33:23 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+*
+ * File: 0_putchar.c
+ * Auth: Ogbonna S Ikechukwu
+ *
+ */
+
+#include "Starford.h"
+
+/**
+ * main - Prints "Starford" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
